a9e89b5318fd51ffd8b2d8f592d8163fecff3745
Cratoinyan <umutciloglu06@gmail.com>
2022-09-02 15:20:45 +0300

    refactor HomePage

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -22,22 +22,11 @@ const getNotes = async () => {
 
 //--------------------------------------Functional Component-------------------------------------
 const HomePage = ({ navigation }: { navigation: any }) => {
-    const [isLoading, setIsLoading] = React.useState(true)
     const [notes, setNotes] = React.useState([]);
     const [onNoteDelete, setOnNoteDelete] = React.useState(0);
 
     const Note = (props: any) => {
-        console.log(props.id);
         return (
-            // <View style={styles.text}>
-            //     <Text style={{ fontSize: 50 }}>
-            //         {props.text}
-            //     </Text>
-            //     <Button title='delete' onPress={() => {
-            //         deleteNote(props.id);
-            //         setOnNoteDelete(key => key + 1);
-            //     }} />
-            // </View>
             <TouchableOpacity
                 style={styles.text}
                 onPress={() => {
@@ -47,8 +36,8 @@ const HomePage = ({ navigation }: { navigation: any }) => {
                         edit: true
                     })
                 }}>
-                <Text style={{ fontSize: 50 }}>
-                    {props.text}
+                <Text style={{ fontSize: 30 }}>
+                    {props.text.length > 20 ? props.text.substr(0, 20) + '...' : props.text}
                 </Text>
             </TouchableOpacity>
         );
@@ -59,18 +48,10 @@ const HomePage = ({ navigation }: { navigation: any }) => {
         <Note text={item.text} id={item.id} />
     );
 
-    const deleteNote = async (id: string) => {
-        const noteIndex = noteList.findIndex((note: any) => note.id == id);
-        noteList.splice(noteIndex, 1);
-        await AsyncStorage.setItem('notes', JSON.stringify(noteList));
-        setNotes(noteList);
-    }
-
     const populateNotes = async () => {
         await getNotes();
         console.log(noteList);
         setNotes(noteList);
-        setIsLoading(false);
     };
 
     React.useEffect(() => {
@@ -78,10 +59,6 @@ const HomePage = ({ navigation }: { navigation: any }) => {
             populateNotes();
         });
     });
-
-    if (isLoading) {
-        return <View><Text>-----LOADING-----</Text></View>
-    }
 
     return (
         <View style={{ flex: 1 }}>
@@ -111,8 +88,9 @@ const styles = StyleSheet.create({
     },
     text: {
         borderWidth: 2,
-        borderRadius: 20,
-        fontSize: 30,
+        borderRadius: 10,
+        fontSize: 15,
+        marginBottom: '5%',
         overflow: 'hidden',
         borderColor: "#393e46"
     },
